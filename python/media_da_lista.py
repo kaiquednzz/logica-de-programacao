52375da81720df753311f48cc17fbcb0a6d430f7
Kaique Diniz <149344005+kaiquednzz@users.noreply.github.com>
2026-05-12 16:13:43 -0300

    12-05-26

--- python/media_da_lista.py
+++ python/media_da_lista.py
@@ -1,8 +1,28 @@
-"""
-Exercício 48
+print ('--=' * 8)
+print('Média de Valores')
+print ('--=' * 8)
 
-Descubra a média de valores de uma lista.
-"""
+while True:
+    try:
+        qtd_numeros = int(input('Digite a quantidade de números: '))
+        if qtd_numeros <= 0:
+            print('ERRO! Digite um valor maior que 0.')
+            continue
+        break
+    except ValueError:
+        print('ERRO! Digite um valor válido.')
 
-# Escreva sua solução abaixo
+numeros = []
+while True:
+    try:
+        for contador in range(qtd_numeros):
+            numero = float(input(f'Digite o {contador + 1} número: '))
+            numeros.append(numero)
+    except ValueError:
+        print('ERRO! Digite um valor válido.')
+        continue
+    break
 
+soma_numeros = sum(numeros)
+media = soma_numeros / qtd_numeros
+print(f'A média é {media}.')    
